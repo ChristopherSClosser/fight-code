f60810ea9bb0fef2b765a96a38faaf3609166ade
Kayla Asay <kdasay16@gmail.com>
2017-02-27 11:50:38 -0800

    radio buttons

--- js/app.js
+++ js/app.js
@@ -6,3 +6,20 @@ function Player(name, difficuly, fighter){
   this.difficuly = difficuly;
   this.character = fighter;
 }
+function Question(question, answers, correctAnswer) {
+  this.question = question;
+  this.answers = answers;
+  this.correctAnswer = correctAnswer;
+}
+var formElement = document.getElementById('entry-form');
+
+formElement.addEventListener('submit', handleSubmit);
+
+function handleSubmit (event){
+  event.preventDefault();
+  event.stopPropagation();
+  console.log(event);
+
+  var userName = event.target.pickName.value;
+  console.log(userName);
+}
